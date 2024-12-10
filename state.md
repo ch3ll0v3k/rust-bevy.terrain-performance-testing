453d699bed7e54ead38d277deb2cd279dc0f33ae
Timoschenko V <ch3ll0v3k@yandex.com>
2024-12-10 17:30:35 +0100

    sync

---
+++ state.md
@@ -0,0 +1,3 @@
+cargo run --features bevy/dynamic_linking => 167 fps
+cargo run --release -- --api wgpu => 170 fps
+
--- x-run-dev.sh
+++ x-run-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# cargo run --release -- --api wgpu
+cargo run --release -- --api wgpu
 # cargo run --features bevy/dynamic_linking --api wgpu
-cargo run --features bevy/dynamic_linking
+# cargo run --features bevy/dynamic_linking
 # cargo run --release  --features bevy/trace_chrome -- --api wgpu
 # cargo run --release --features bevy/dynamic_linking -- --api wgpu
